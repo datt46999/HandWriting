6d41c6e675349f9d7d92e392dd608f998c2aff61
DatNguyen2003 <125117718+datt46999@users.noreply.github.com>
2026-03-25 22:06:38 +0700

    Update train_transformers.py

--- utils/train_transformers.py
+++ utils/train_transformers.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import random_split, DataLoader
 from datasets import load_metric
 from transformers import AdamW
-
+from evaluate import load as load_metric 
 from utils.utils import Logger, modeluse
 from data.iamdataset import process
 from tqdm  import tqdm
@@ -12,16 +12,16 @@ import os
 
 
 
-cer_metric = load_metric("cer")
+
 
 def select_dataloader(dataset, cfg):
     train_size = cfg.train_size
-    val_size = cfg.val_size
-
-    train_set, val_set = random_split(dataset, [int(len(dataset) * train_size)+1, int(len(dataset) *val_size)])
-    train_dataloder = DataLoader(train_set, batch_size = 4, shuffle = True)
-    val_dataloder = DataLoader(val_set, batch_size = 4)
-    return train_dataloder, val_dataloder
+    train_len = int(len(dataset) * train_size)
+    val_len = len(dataset) - train_len
+    train_set, val_set = random_split(dataset, [train_len, val_len])
+    train_loader = DataLoader(train_set, batch_size = 4, shuffle = True)
+    val_loader = DataLoader(val_set, batch_size = 4, shuffle = False)
+    return train_loader, val_loader
 
 def sel_attributes(model, processor, cnfg):
     model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
@@ -37,29 +37,44 @@ def sel_attributes(model, processor, cnfg):
     model.config.length_penalty = cnfg.length_penalty
     model.config.num_beams = cnfg.num_beams
 
+def newemd(model, processor, cnfg):
+    new_emb = model.decoder.resize_token_embeddings(len(processor.tokenizer))
+    print(f"New embedding size: {new_emb}")
+    # Adjust our image size and output sequence lengths
+    model.config.encoder.image_size = (cnfg.height, cnfg.width)
+    model.config.decoder.max_length = 384
 
-
-def computer_cer(pred_ids, label_ids, processor):
+cer_metric = load_metric("cer")
+def compute_cer(pred_ids, label_ids, processor):
     pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+
+    label_ids = label_ids.clone()
     label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
     label_str = processor.batch_decode(label_ids, skip_special_tokens=True)
 
     cer = cer_metric.compute(predictions=pred_str, references=label_str)
-
     return cer
-def Validation(i,model, val_dataloader, processor, device):
+
+
+def Validation(model, val_dataloader, processor, device):
     model.eval()
     val_cer = 0.0
-    with torch.no_grad():
-        for  batch in tqdm(val_dataloader):
-            output = model.generate[batch["pixel_val"]].to(device)
-            cer = computer_cer(output, batch["labels"], processor)
-            val_cer += cer
-    
-    cur_cer = val_cer/ len(val_dataloader)
-    return cur_cer
-    
 
+    with torch.no_grad():
+        for batch in tqdm(val_dataloader):
+            outputs = model.generate(
+                batch["pixel_values"].to(device),
+            )
+
+            v_cer = compute_cer(
+                pred_ids=outputs,
+                label_ids=batch["labels"],
+                processor=processor
+            )
+
+            val_cer += v_cer
+
+    return val_cer / len(val_dataloader)
 
 
 class Transformer_trainning:
@@ -76,7 +91,7 @@ class Transformer_trainning:
 
     def train(self):
 
-        self.model.train()
+        # self.model.train()
         max_epochs = self.cfg.max_epochs
         logg = Logger("Train",max_epochs, len(self.train_dataloader), self.device )
         for epoch in range(max_epochs):
@@ -93,16 +108,17 @@ class Transformer_trainning:
                 loss.backward()
                 self.optimizer.step()
                 self.optimizer.zero_grad()
-                self.optimizer.step()
+                # self.optimizer.step()
                 train_loss += loss.item() 
 
-                if (i+1)% self.cfg.display_step ==0:
-                    logg.log_iteration(epoch + 1, (i+1) * self.cfg.display_step, loss.item())
+                if (i+1)% 1000 ==0:
+                    # print(f"Train: {epoch+1} ")
+                    logg.log_iteration(epoch + 1, (i+1), loss.item())
             print(f"Done epoch #{epoch+1}, time for this epoch: {time.time()-start_time}s")
             train_loss/= (i + 1)
             
 
-            cur_cer = Validation(i,self.model, self.val_dataloader, self.processor, self.device)
+            cur_cer = Validation(self.model, self.val_dataloader, self.processor, self.device)
             logg.log_metrics(epoch, cur_cer)
             os.makedirs(os.path.dirname(self.cfg.save_model), exist_ok= True)
             test_loss/= (i+1)
@@ -113,5 +129,6 @@ class Transformer_trainning:
         torch.save(self.model.state_dict(), final_path)
         print(f"Final model saved at {final_path}")
     def test(self):
+        # cur_cer = Validation(self.model, self.val_dataloader, self.processor, self.device)
         pass
 
